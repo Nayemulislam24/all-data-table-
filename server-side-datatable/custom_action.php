611e2b72597aee302a38c75932885312926796c6
MD Nayem <nayemtp@gmail.com>
2023-11-03 23:33:48 +0600

    data table

---
+++ server-side-datatable/custom_action.php
@@ -0,0 +1,62 @@
+<?php 
+## database connection
+$mysqli = new mysqli("localhost", "root", "", "asterisk");
+
+## Check connection
+if ($mysqli->connect_errno) {
+    echo json_encode("Failed to connect to MySQL");
+    exit();
+}
+
+## define table name
+$table = "students";
+
+## define columns
+$columns = array(
+    0 => "id",
+    1 => "name",
+    2 => "roll",
+);
+
+## request with parameters, define variables
+$params = array(); $params = $_REQUEST; $where = "";
+$recordsTotal = $recordsFiltered = 0;
+
+## define conditions
+if (!empty($params['search']['value'])) {
+    $where = " WHERE `name` LIKE '%".$params['search']['value']."%' OR `email` LIKE '%".$params['search']['value']."%'";
+}
+
+## convert column array to select value
+$select = implode("`, `", $columns);
+
+## fetch data
+$user_result = mysqli_query($mysqli, "SELECT `".$select."` FROM ".$table." ".$where." ORDER BY ".$columns[$params['order'][0]['column']]." ".$params['order'][0]['dir']." LIMIT ".$params['start'].",".$params['length']."");
+
+## if data found set array for response
+$json_data = array();
+if (mysqli_num_rows($user_result) > 0) {
+    ## total count for recordsTotal & recordsFiltered
+    $tot_sql = mysqli_query($mysqli, "SELECT * FROM ".$table." ".$where."");
+    $recordsTotal = mysqli_num_rows($tot_sql);
+    $recordsFiltered = mysqli_num_rows($tot_sql);
+
+    ## fetch data for response
+    while ($user_data = mysqli_fetch_row($user_result)) {
+        $data = array();
+        $data[] = $user_data[0];
+        $data[] = $user_data[1];
+        $data[] = $user_data[2];
+        $json_data[] = $data;
+    }
+}
+
+## return data to view
+echo json_encode(array(
+    "draw" => intval($params['draw']),
+    "recordsTotal" => intval($recordsTotal),
+    "recordsFiltered" => intval($recordsFiltered),
+    "data" => $json_data
+));
+
+?>
---
+++ server-side-datatable/datatable_action.php
@@ -0,0 +1,51 @@
+<?php 
+// Database connection info 
+$dbDetails = array( 
+    'host' => 'localhost', 
+    'user' => 'root', 
+    'pass' => '', 
+    'db'   => 'asterisk' 
+); 
+ 
+// DB table to use 
+$table = 'students'; 
+ 
+// Table's primary key 
+$primaryKey = 'id'; 
+ 
+## $row single row return from database. 
+## The `db` parameter represents the column name in the database.  
+## The `dt` parameter represents the DataTables column identifier. 
+
+
+
+$columns = array(
+    0 => "id",
+    1 => "name",
+    2 => "roll",
+);
+
+$columns = array( 
+    array('db' => 'id', 'dt' => 0), 
+    array('db' => 'name',  'dt' => 1), 
+    array( 'db' => 'roll', 'dt' => 2, 
+        // 'formatter' => function( $d, $row) { 
+        //     return ($d == 1) ? 'Active' : 'Inactive'; 
+        // } 
+    ) 
+); 
+
+
+
+
+
+
+
+ 
+## Include SQL query processing class 
+require 'ssp.class.php'; 
+ 
+## Output data as json format 
+echo json_encode( 
+    SSP::simple($_GET, $dbDetails, $table, $primaryKey, $columns) 
+);
